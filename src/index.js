4137f88c8076826544ad8de2a9c762d7fab71e51
Alex Hinds <alex.hinds141@gmail.com>
2019-03-27 16:53:27 +1100

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import { render, hydrate } from 'react-dom'
+
+import './index.css'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+    hydrate(<App />, rootElement);
+} else {
+    render(<App />, rootElement);
+}
