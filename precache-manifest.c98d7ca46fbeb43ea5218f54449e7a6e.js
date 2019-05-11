9f9941a5d48876a8fbc3deea3996856ef246233a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-11 08:56:55 +0000

    Deploy DarkPurple141/deploy-tutorial to github.com/DarkPurple141/deploy-tutorial.git:gh-pages

---
+++ precache-manifest.c98d7ca46fbeb43ea5218f54449e7a6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f52f13796e5cc582a72",
+    "url": "/deploy-tutorial/static/js/runtime~main.5a9dddf7.js"
+  },
+  {
+    "revision": "0fdf347fb5465e0f29cf",
+    "url": "/deploy-tutorial/static/js/main.6869bce8.chunk.js"
+  },
+  {
+    "revision": "970e9aab41795d71d015",
+    "url": "/deploy-tutorial/static/js/2.9cc6eec3.chunk.js"
+  },
+  {
+    "revision": "0fdf347fb5465e0f29cf",
+    "url": "/deploy-tutorial/static/css/main.9d4a0cc6.chunk.css"
+  },
+  {
+    "revision": "1858eceed6ac49be958c7034d22f8a39",
+    "url": "/deploy-tutorial/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/deploy-tutorial/precache-manifest.c98d7ca46fbeb43ea5218f54449e7a6e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/deploy-tutorial/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
